857a8c148012d82a430ddfca443fcf911a516ed7
NataliaAraujo2 <102322514+NataliaAraujo2@users.noreply.github.com>
2022-09-15 19:38:28 -0300

    Cria o file javascript com o cálculo

---
+++ app.js
@@ -0,0 +1,35 @@
+const calcular = document.getElementById("calculate");
+
+function imc() {
+  const name = document.getElementById("name").value;
+  const height = document.getElementById("height").value;
+  const weight = document.getElementById("weight").value;
+  const result = document.getElementById("result");
+  }
+
+  if (name !== "" && height !== "" && weight !== "") {
+    const valorIMC = (weight / (height * height)).toFixed(1);
+
+    let classificacao = "";
+
+    if (valorIMC < 18.5) {
+      classificacao = "abaixo do peso.";
+    } else if (valorIMC < 25) {
+      classificacao = "com peso ideal. Parabéns!!!";
+    } else if (valorIMC < 30) {
+      classificacao = "levemente acima do peso.";
+    } else if (valorIMC < 35) {
+      classificacao = "com obesidade grau I.";
+    } else if (valorIMC < 40) {
+      classificacao = "com obesidade grau II.";
+    } else {
+    classificacao = "com obesidade grau III. Cuidado!!";
+    }
+    result.textContent=`${name} Seu IMC é ${valorIMC}! E você está ${classificacao}`;
+  } else {
+    result.textContent = "Preencha todos os campos!"
+  }
+  
+
+
+calcular.addEventListener ("click",imc)
